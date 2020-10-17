a3e84056892f0440226aba7fbca058536fab1278
Antoine Nwyn <Nick1234567890a@gmail.com>
2020-10-17 13:25:37 -0400

    Created SavingsAccount.cs

---
+++ BankAccount/Resources/SavingsAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount.Resources
+{
+    class SavingsAccount : Account
+    {
+        protected AccountStatus account;
+
+        public SavingsAccount(double balance, double interest) : base(balance, interest) { }
+
+        public void MakeWithdrawal(double amount)
+        {
+            if(account == AccountStatus.Inactive)
+            {
+                base.MakeWithdrawal(amount);
+            }
+        }
+        public void MakeDeposit(double amount)
+        {
+
+            base.MakeDeposit(amount);
+            if (account == AccountStatus.Inactive && base.CurrentBalance > 25)
+            {
+                account = AccountStatus.Active;
+
+            }
+
+        }
+    }
+}
